3f30c2b4f306bff6f6302d7b9a66ab274615324d
Rodrigo Vargas Honorato <r.vargashonorato@uu.nl>
2024-05-02 11:05:54 +0200

    fix system variable error (#9)

--- src/proabc_2/__init__.py
+++ src/proabc_2/__init__.py
@@ -1,22 +1,22 @@
 import os
 
-HMMER_PATH = os.environ["HMMER_PATH"]
-IGBLAST_PATH = os.environ["IGBLAST_PATH"]
-IGDATA = os.environ["IGDATA"]
+HMMER_PATH = os.environ.get("HMMER_PATH")
+IGBLAST_PATH = os.environ.get("IGBLAST_PATH")
+IGDATA = os.environ.get("IGDATA")
 
-HELP_MSG = "Please check the installation instructions at <https://github.com/haddocking/proabc-2>"
+HELP_MSG = "Check the installation instructions of THIRD-PARTY dependencies at `https://github.com/haddocking/proabc-2`"
 
 if HMMER_PATH is None:
-    _msg = "Please set the HMMER_PATH environment variable" + os.newline + HELP_MSG
+    _msg = "Please set the HMMER_PATH environment variable" + os.linesep + HELP_MSG
     print(_msg)
     exit(1)
 
 if IGBLAST_PATH is None:
-    _msg = "Please set the IGBLAST_PATH environment variable" + os.newline + HELP_MSG
+    _msg = "Please set the IGBLAST_PATH environment variable" + os.linesep + HELP_MSG
     print(_msg)
     exit(1)
 
 if IGDATA is None:
-    _msg = "Please set the IGDATA environment variable" + os.newline + HELP_MSG
+    _msg = "Please set the IGDATA environment variable" + os.linesep + HELP_MSG
     print(_msg)
     exit(1)
